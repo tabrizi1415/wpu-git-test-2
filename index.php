cc30ee2ba09f8fa5b11115d59310498fd2d721ce
tabrizi1415 <oitnet4@gmail.com>
2023-07-16 21:54:16 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores dolor magnam sed veniam blanditiis adipisci magni modi, aliquam ex quo.</p>
 </body>
 </html>
